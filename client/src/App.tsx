e8fe747a49acc83484decfcfeff43e66cacd075d
Lukas Vidfar <lgkvidfar@gmail.com>
2022-02-02 15:24:03 +0400

    git_small_regular_update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { server } from "./config";
+
+const App = () => {
+    const handleLogin = () => {
+        console.log("sending here");
+
+        axios.get(`${server.baseUrl}/register`).then(response => {
+            console.log(response);
+        });
+    };
+    return (
+        <div className="App">
+            <button onClick={handleLogin}>log in</button>
+        </div>
+    );
+};
 
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,44 +1,47 @@
-import { Cookies } from '@interfaces';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express, { Response } from 'express';
-import { authAdmin, authMiddleWare } from './middlewares/auth-middleware';
-import { getGitHubUser } from './auth/github-auth';
+import { Cookies } from "@interfaces";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Response } from "express";
+import { authAdmin, authMiddleWare } from "./middlewares/auth-middleware";
+import { getGitHubUser } from "./auth/github-auth";
 import {
     buildTokens,
     clearTokens,
     refreshTokens,
     setTokens,
     verifyRefreshToken,
-} from './auth/token-utils';
-import { server } from './config';
-import connectToMongoDB from './database/connect';
+} from "./auth/token-utils";
+import { server } from "./config";
+import connectToMongoDB from "./database/connect";
 import {
     createUser,
     getUserByGitHubId,
     getUserById,
     increaseTokenVersion,
-} from './services/user-service';
+} from "./services/user-service";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+app.use(cors());
 app.use(cookieParser());
 
-app.get('/', (req, res, next) => {
-    console.log('api is healthy');
+app.get("/register", userRouter);
 
-    res.json({ message: 'api is healthy' });
+app.get("/", (req, res, next) => {
+    console.log("api is healthy");
+
+    res.json({ message: "api is healthy" });
     next();
 });
 
-app.get('/github', async (req, res) => {
+app.get("/github", async (req, res) => {
     const { code } = req.query;
 
     const gitHubUser = await getGitHubUser(code as string);
     let user = await getUserByGitHubId(gitHubUser.id);
     if (!user) {
-        console.log('creating user from github user:', gitHubUser);
+        console.log("creating user from github user:", gitHubUser);
 
         user = await createUser({
             name: gitHubUser.name,
@@ -55,7 +58,7 @@ app.get('/github', async (req, res) => {
             cart: null,
         });
     } else {
-        console.log('this time a user was found!', user);
+        console.log("this time a user was found!", user);
     }
 
     const { accessToken, refreshToken } = buildTokens(user);
@@ -64,44 +67,44 @@ app.get('/github', async (req, res) => {
     res.redirect(`${process.env.CLIENT_URL}/me`);
 });
 
-app.post('/refresh', async (req, res) => {
+app.post("/refresh", async (req, res) => {
     try {
         const current = verifyRefreshToken(req.cookies[Cookies.RefreshToken]);
-        if (!current) throw 'current refresh token not found';
-        console.log('this is current in server/index @ post(refresh): ', current);
+        if (!current) throw "current refresh token not found";
+        console.log("this is current in server/index @ post(refresh): ", current);
         const user = await getUserById(current.userId);
-        if (!user) throw 'user not found';
-        console.log('this is user in server/index @ post(refresh): ', user);
+        if (!user) throw "user not found";
+        console.log("this is user in server/index @ post(refresh): ", user);
         const { refreshedAccessToken, refreshedRefreshToken } = refreshTokens(
             current,
             user.tokenVersion
         );
         setTokens(res, refreshedAccessToken, refreshedRefreshToken);
     } catch (e) {
-        console.log('error in server/index @ post(refresh)', e);
+        console.log("error in server/index @ post(refresh)", e);
         clearTokens(res);
     }
     res.end();
 });
 
-app.post('/logout', authMiddleWare, (req, res) => {
+app.post("/logout", authMiddleWare, (req, res) => {
     clearTokens(res);
     res.end();
 });
 
-app.post('/logout-all', authMiddleWare, async (req, res: Response) => {
+app.post("/logout-all", authMiddleWare, async (req, res: Response) => {
     await increaseTokenVersion(res.locals.token.userId);
     clearTokens(res);
     res.end();
 });
 
-app.get('/me', authMiddleWare, async (req, res) => {
-    console.log('this is res.locals', res.locals);
+app.get("/me", async (req, res) => {
+    console.log("this is res.locals", res.locals);
 
     const user = await getUserById(res.locals.token.userId);
 
-    console.log('this is res.locals', res.locals);
-    console.log('this is the currently authenticated user', user);
+    console.log("this is res.locals", res.locals);
+    console.log("this is the currently authenticated user", user);
 
     res.json(user);
 });
@@ -109,7 +112,7 @@ app.get('/me', authMiddleWare, async (req, res) => {
 const main = async () => {
     await connectToMongoDB();
     app.listen(server.port, () => {
-        console.log('listening on port', server.port);
+        console.log("listening on port", server.port);
     });
 };
 
